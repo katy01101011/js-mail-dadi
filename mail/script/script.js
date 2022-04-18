fa1ea2e3216af0ec56cbc19bdbae7ea7aa3bd873
katy01101011 <katy.91560@gmail.com>
2022-04-18 15:40:59 +0200

    Mail stampato in html

--- mail/script/script.js
+++ mail/script/script.js
@@ -1,1 +1,35 @@
-alert("Ciao!")
+const loginMails = [
+    'ciao@gmail.com',
+    'pinco@gmail.com',
+    'pallino@gmail.com'
+];
+
+// Chiedi all’utente la sua email
+let userMail = prompt('Inserisci la tua email')
+console.log(userMail);
+
+// Controlla che sia nella lista di chi può accedere
+let result = false;
+
+for (let i = 0; i < loginMails.length; i++) {
+    const email = loginMails[i];
+    console.log(email);
+    if (userMail === loginMails[i]) {
+        result = true;
+        console.log(result);
+    }
+}
+
+// Stampa un messaggio appropriato sull’esito del controllo
+if (result === true) {
+    const answer = document.createElement('h1');
+    answer.innerHTML = 'Accesso consentito';
+    document.body.appendChild(answer);
+    answer.classList.add('green');
+
+} else {
+    const answer = document.createElement('h1');
+    answer.innerHTML = 'Accesso negato';
+    document.body.appendChild(answer);
+    answer.classList.add('red');
+}
